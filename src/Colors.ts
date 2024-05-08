40c56fc2c82326a0bc89325448f06611344190d7
Andy <onsubmit@outlook.com>
2024-05-07 20:19:33 -0700

    Add colorful color

--- src/Colors.ts
+++ src/Colors.ts
@@ -4,4 +4,13 @@ export class Colors {
   static black: ColorAtTimeFn = (_time: DOMHighResTimeStamp) => '#000';
   static lightGrey: ColorAtTimeFn = (_time: DOMHighResTimeStamp) => '#ccc';
   static veryLightGrey: ColorAtTimeFn = (_time: DOMHighResTimeStamp) => '#eee';
+  static colorful: ColorAtTimeFn = (time: DOMHighResTimeStamp) => {
+    const r = 128 * (0.5 + Math.cos(time / 1000) / 2);
+    const g = 200 * (0.5 + Math.sin(time / 1000) / 2);
+    const b = 255;
+    const a = 1;
+
+    const color = 'rgba(' + [r, g, b, a].join(',') + ')';
+    return color;
+  };
 }
